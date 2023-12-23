145725236805974ce5344c62fe281d79d64bdb56
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2023-12-23 15:18:36 +0200

    make the HealYourCropAndPlantText in home

--- lib/Feature/home/presentation/widgets/home_view_body.dart
+++ lib/Feature/home/presentation/widgets/home_view_body.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:smartsoil/Feature/home/presentation/widgets/heal_your_crop_and_plant_text.dart';
 import 'package:smartsoil/core/helper/spacing.dart';
 import 'package:smartsoil/core/themaing/app_colors.dart';
@@ -17,16 +15,6 @@ class HomeViewBody extends StatefulWidget {
 }
 
 class _HomeViewBodyState extends State<HomeViewBody> {
-  final ImagePicker picker = ImagePicker();
-  File? image;
-
-  pickedImage() async {
-    XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      image = File(pickedFile!.path);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -51,97 +39,7 @@ class _HomeViewBodyState extends State<HomeViewBody> {
                 verticalSpacing(40),
                 const HealYourCropAndPlantText(),
                 verticalSpacing(16),
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  width: 343.w,
-                  height: 186.h,
-                  decoration: BoxDecoration(
-                    color: ColorManger.whiteColor,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        color: ColorManger.greyColor.withOpacity(0.2),
-                        spreadRadius: 2.5,
-                        blurRadius: 15,
-                        offset: const Offset(3, 0),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  ImagesAssetsManger.cameraImage,
-                                  width: 52.w,
-                                  height: 52.h,
-                                ),
-                                verticalSpacing(8),
-                                const Text(
-                                  'Take a\npicture',
-                                  textAlign: TextAlign.center,
-                                )
-                              ],
-                            ),
-                          ),
-                          const Icon(
-                            Icons.arrow_forward_ios,
-                            size: 19,
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  ImagesAssetsManger.phoneImage,
-                                  width: 52.w,
-                                  height: 52.h,
-                                ),
-                                verticalSpacing(8),
-                                const Text(
-                                  'Get the\nresult',
-                                  textAlign: TextAlign.center,
-                                )
-                              ],
-                            ),
-                          ),
-                          const Icon(
-                            Icons.arrow_forward_ios,
-                            size: 19,
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  ImagesAssetsManger.diagnosisImage,
-                                  width: 52.w,
-                                  height: 52.h,
-                                ),
-                                verticalSpacing(8),
-                                const Text(
-                                  'Get \ndiagnosis',
-                                  textAlign: TextAlign.center,
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      verticalSpacing(16),
-                      Expanded(
-                        child: CustomBottom(
-                          backgroundColor: ColorManger.primaryColor,
-                          bottomtext: 'Open Gallary',
-                          onPressed: () {
-                            pickedImage();
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                const GuidanceUserToHealHerCrop()
               ],
             ),
           ),
@@ -150,3 +48,118 @@ class _HomeViewBodyState extends State<HomeViewBody> {
     );
   }
 }
+
+class GuidanceUserToHealHerCrop extends StatefulWidget {
+  const GuidanceUserToHealHerCrop({super.key});
+
+  @override
+  State<GuidanceUserToHealHerCrop> createState() =>
+      _GuidanceUserToHealHerCropState();
+}
+
+class _GuidanceUserToHealHerCropState extends State<GuidanceUserToHealHerCrop> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      width: 343.w,
+      height: 186.h,
+      decoration: BoxDecoration(
+        color: ColorManger.whiteColor,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: ColorManger.greyColor.withOpacity(0.2),
+            spreadRadius: 2.5,
+            blurRadius: 15,
+            offset: const Offset(3, 0),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      ImagesAssetsManger.cameraImage,
+                      width: 52.w,
+                      height: 52.h,
+                    ),
+                    verticalSpacing(8),
+                    const Text(
+                      'Take a\npicture',
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+              const Icon(
+                Icons.arrow_forward_ios,
+                size: 19,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      ImagesAssetsManger.phoneImage,
+                      width: 52.w,
+                      height: 52.h,
+                    ),
+                    verticalSpacing(8),
+                    const Text(
+                      'Get the\nresult',
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+              const Icon(
+                Icons.arrow_forward_ios,
+                size: 19,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      ImagesAssetsManger.diagnosisImage,
+                      width: 52.w,
+                      height: 52.h,
+                    ),
+                    verticalSpacing(8),
+                    const Text(
+                      'Get \ndiagnosis',
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          verticalSpacing(16),
+          Expanded(
+            child: CustomBottom(
+              backgroundColor: ColorManger.primaryColor,
+              bottomtext: 'Heal Your Crops',
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+//Fuction of Open Gallery
+// ----------------------------------------------------------------
+  // final ImagePicker picker = ImagePicker();
+  // File? image;
+
+  // pickedImage() async {
+  //   XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+  //   setState(() {
+  //     image = File(pickedFile!.path);
+  //   });
+  // }
+  // pickedImage();
