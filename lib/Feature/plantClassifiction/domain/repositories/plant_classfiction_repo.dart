ff01f5a62e94994da17ab213535dbbb33f4953ec
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-02-18 07:36:34 +0200

    make  plant care repo and plant care repo impl

--- lib/Feature/plantClassifiction/domain/repositories/plant_classfiction_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:image_picker/image_picker.dart';
-
-abstract class PlantClassificationRepo {
-  Future<File?> pickedImageFromGallary(ImagePicker picker);
-  Future<File?> pickedImageFromCamira(ImagePicker picker);
-}
