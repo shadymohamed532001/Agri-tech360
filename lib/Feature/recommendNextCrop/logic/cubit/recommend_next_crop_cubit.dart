e2a36d8c5565f3a4165e53e5beb3d7ed404a003c
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-03-20 01:54:10 +0200

    Add static method to get RecommendNextCropCubit object
    Added a static method to get the RecommendNextCropCubit object from the context
    in order to improve code readability and reusability.

--- lib/Feature/recommendNextCrop/logic/cubit/recommend_next_crop_cubit.dart
+++ lib/Feature/recommendNextCrop/logic/cubit/recommend_next_crop_cubit.dart
@@ -1,8 +1,38 @@
-import 'package:bloc/bloc.dart';
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:smartsoil/Feature/recommendNextCrop/data/models/next_crop_model.dart';
+import 'package:smartsoil/Feature/recommendNextCrop/domain/repositories/recommend_next_crop_repo.dart';
 
 part 'recommend_next_crop_state.dart';
 
 class RecommendNextCropCubit extends Cubit<RecommendNextCropState> {
-  RecommendNextCropCubit() : super(RecommendNextCropInitial());
+  RecommendNextCropCubit({required this.recommendNextCropRepo})
+      : super(RecommendNextCropInitial());
+  final RecommendNextCropRepo recommendNextCropRepo;
+
+  static RecommendNextCropCubit getObject(context) =>
+      BlocProvider.of<RecommendNextCropCubit>(context);
+  File? image;
+  Future<void> uploadImageFromGalleryModel({required ImagePicker picke}) async {
+    try {
+      image = await recommendNextCropRepo.pickedImageFromGallary(picke);
+
+      emit(UploadImageFromGallerySucsessState(image: image!));
+    } catch (e) {
+      emit(UploadImageErrorState(errorMessage: e.toString()));
+    }
+  }
+
+  Future<void> uploadImageFromCameraModel({required ImagePicker picke}) async {
+    try {
+      image = await recommendNextCropRepo.pickedImageFromCamira(picke);
+
+      emit(UploadImageFromCameraSucsessState(image: image!));
+    } catch (e) {
+      emit(UploadImageErrorState(errorMessage: e.toString()));
+    }
+  }
 }
