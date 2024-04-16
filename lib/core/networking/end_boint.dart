2f27f2c471589063ff84320a0b2b7c42c5ad37f5
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-16 16:22:50 +0200

    Update API endpoint URLs and add PUT request method
    Changed the base URL in end_boint.dart and added a new PUT request method in
    api_services.dart to update user data. This commit enhances the API services
    functionality.

--- lib/core/networking/end_boint.dart
+++ lib/core/networking/end_boint.dart
@@ -1,4 +1,4 @@
-const baseUrl = 'https://5269-156-218-0-46.ngrok-free.app/';
+const baseUrl = 'https://bef5-156-203-179-237.ngrok-free.app/';
 
 const loginendpoint = 'login';
 
@@ -31,3 +31,5 @@ const recommendcropendpoint = 'recommend-crop';
 const chatendpoint = 'chat';
 
 const userDataendpoint = 'user-data';
+
+const updateuserdataendpoint = 'update-user-data';
